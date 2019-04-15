c0e70b425ef0467750a6b059adc9eb1220e8d1c8
balraj <erbalrajs@gmail.com>
2019-04-15 15:39:07 +0530

    Added use case to get filtered data

--- FeedbackAnalytics/Usecases/FeedbackDetailsUsecase.swift
+++ FeedbackAnalytics/Usecases/FeedbackDetailsUsecase.swift
@@ -7,8 +7,35 @@
 //
 
 import Foundation
+import PromiseKit
 
 public struct FeedbackDetailsUsecase {
   public init() {}
   
+  public func getFeedbackDetailsGroupedByPlatform(between: (startDate: Int64, endDate: Int64)) -> Promise<[String : [FeedbackItem]]> {
+    return AppEnvironment.current.apiService.fetchFeedbackDetails()
+      .map { (response: FeedbackDetailsResponse) -> [String : [FeedbackItem]] in
+        return response.items.map { FeedbackItem(item: $0) }
+          |> ((between
+            |> FeedbackQuery.filterByDate) >>>= FeedbackQuery.groupByPlatform().run).run
+    }
+  }
+  
+  public func getFeedbackDetailsGroupedByRating(between: (startDate: Int64, endDate: Int64)) -> Promise<[Int : [FeedbackItem]]> {
+    return AppEnvironment.current.apiService.fetchFeedbackDetails()
+      .map { (response: FeedbackDetailsResponse) -> [Int : [FeedbackItem]] in
+        return response.items.map { FeedbackItem(item: $0) }
+          |> ((between
+            |> FeedbackQuery.filterByDate) >>>= FeedbackQuery.groupByRating().run).run
+    }
+  }
+  
+  public func getFeedbackDetailsGroupedByDates(between: (startDate: Int64, endDate: Int64)) -> Promise<[Date : [FeedbackItem]]> {
+    return AppEnvironment.current.apiService.fetchFeedbackDetails()
+      .map { (response: FeedbackDetailsResponse) -> [Date : [FeedbackItem]] in
+        return response.items.map { FeedbackItem(item: $0) }
+          |> ((between
+            |> FeedbackQuery.filterByDate) >>>= FeedbackQuery.groupByCreatedDate().run).run
+    }
+  }
 }
